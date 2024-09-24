3e0bdbfa0bc043a4c4f0f09c264d7f3cd06a2954
Kayo Werter <102062349+Kayo-Werter@users.noreply.github.com>
2024-09-24 14:23:24 -0300

    acrescetando campo de descrição e local na troca de peças

--- replacement/models.py
+++ replacement/models.py
@@ -6,6 +6,8 @@ class Replacement(models.Model):
     exchanged_part = models.CharField(max_length=255)
     value_part = models.DecimalField(default=0, max_digits=20, decimal_places=2)
     replacement_day = models.DateField(auto_now=True)
+    description = models.TextField(null=True, blank=True)
+    local = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return f'{self.vehicle} - {self.value_part}'
