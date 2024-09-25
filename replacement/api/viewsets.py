fb9bd9a4eb1e19750d894ed11f0231f93e0b099c
Kayo Werter <102062349+Kayo-Werter@users.noreply.github.com>
2024-09-25 17:45:31 -0300

    filtros nos endpoints

--- replacement/api/viewsets.py
+++ replacement/api/viewsets.py
@@ -1,3 +1,5 @@
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from replacement.models import Replacement
 from .serializers import ReplacementSerializer
@@ -6,3 +8,6 @@ from .serializers import ReplacementSerializer
 class ReplacementViewSet(viewsets.ModelViewSet):
     queryset = Replacement.objects.all()
     serializer_class = ReplacementSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['vehicle']
+    
