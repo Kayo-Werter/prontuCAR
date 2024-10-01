9f715cd1aede3c642d645fc1f3c796aaad177c10
israel2023 <mc.israel.almeida@gmail.com>
2024-10-01 20:26:15 -0300

    models de vehicle

--- setup/settings.py
+++ setup/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'prontucar',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '5432',
     }
--- vehicle/models.py
+++ vehicle/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from document.models import Document
+from django.core.validators import FileExtensionValidator
 
 
 class Vehicle(models.Model):
@@ -12,7 +12,7 @@ class Vehicle(models.Model):
     automobile = models.CharField(choices=automoveis, max_length=255)
     name = models.CharField(max_length=255)
     plate = models.CharField(max_length=255, null=True, blank=True)
-    document = models.ForeignKey(Document, null=True, blank=True, on_delete=models.CASCADE, related_name='vehicles')
+    file = models.FileField(null=True, blank=True, upload_to='documents/', validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
     buy_day = models.DateField(default=timezone.now)
 
 
