45a8a149145f7c62e4854a81fe5f60e40188acfd
EmmanuelVictorADS <115032984+EmmanuelVictorADS@users.noreply.github.com>
2024-10-09 13:17:11 -0300

    Sidebar e Tela de Configurações
    O ajuste sugerido da sidebar foi implementado (Ela agora empurra o conteúdo da página), e uma versão básica da tela de configurações foi feita.

--- frontend/src/app/components/LayoutWrapper.tsx
+++ frontend/src/app/components/LayoutWrapper.tsx
@@ -1,9 +1,8 @@
 
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { usePathname } from "next/navigation";
 import { Sidebar } from "./Sidebar";
-
 
 interface LayoutWrapperProps {
   children: React.ReactNode;
@@ -11,6 +10,7 @@ interface LayoutWrapperProps {
 
 const LayoutWrapper: React.FC<LayoutWrapperProps> = ({ children }) => {
   const pathname = usePathname();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   // Defina as rotas onde a Sidebar não deve aparecer
   const noSidebarPaths = ["/telaLogin", "/telaCadastro"];
@@ -18,8 +18,9 @@ const LayoutWrapper: React.FC<LayoutWrapperProps> = ({ children }) => {
 
   return (
     <div className="min-h-screen flex bg-blue-100">
-      {shouldRenderSidebar && <Sidebar />}
-      <div className={`flex-1 ${shouldRenderSidebar ? "pl-16 md:pl-30" : ""}`}>
+      {shouldRenderSidebar && <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />}
+      {/* Ajuste dinâmico */}
+      <div className={`flex-1 transition-all duration-300 ${isSidebarOpen ? "pl-64" : "pl-16"}`}>
         {children}
       </div>
     </div>
