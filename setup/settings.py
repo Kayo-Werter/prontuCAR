94ef8f74d8854ac816932ca6ce85c14aba945dfb
Kayo Werter <102062349+Kayo-Werter@users.noreply.github.com>
2024-10-09 11:16:50 -0300

    log

--- setup/settings.py
+++ setup/settings.py
@@ -154,6 +154,26 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # VARIÁVEIS DE AMBIENTE DE INTEGRAÇÕES
 API_KEY_OPENWEATHERMAP = config('API_KEY_OPENWEATHERMAP')
 
+# LOGGIN
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
+
 
 # CONFIGURAÇÕES PARA AUTENTICAÇÃO JWT
 REST_FRAMEWORK = {
